bf2ec72d27b1401a2cf32059b2075370a14db985
RyuJaeHwi <91930771+RyuJaeHwi@users.noreply.github.com>
2025-12-31 21:24:38 +0900

    Title: 입력받아 계산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251231/입력받아 계산/input-calculate.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+
+
+    int a2 = a + 2;
+
+    cout << a2;
+
+    return 0;
+}
