c659b45d8df9c0f6d6a2f9619fea4aaa10dbb668
RyuJaeHwi <91930771+RyuJaeHwi@users.noreply.github.com>
2026-01-01 22:01:41 +0900

    Title: 세 실수의 반올림, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260101/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a;
+    double b;
+    double c;
+
+    cin >> a >> b >> c;
+
+    cout << fixed;
+    cout.precision(3);
+
+    cout << a;
+    cout << "\n";
+    cout << b;
+    cout << "\n";
+    cout << c;
+
+    return 0;
+}
