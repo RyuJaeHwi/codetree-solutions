989b80a5794a60692241635b9c4fa0c087287e8a
RyuJaeHwi <91930771+RyuJaeHwi@users.noreply.github.com>
2026-01-01 22:18:02 +0900

    Title: 합과 평균, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260101/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double A;
+    double B;
+
+    cin >> A >> B;
+
+    double result = (A + B) / 2;
+
+    cout << A + B << " " << result;
+
+    return 0;
+}
