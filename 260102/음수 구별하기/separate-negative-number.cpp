a6961dc0025ba2755256e32795dc6e49081a9490
RyuJaeHwi <91930771+RyuJaeHwi@users.noreply.github.com>
2026-01-02 15:36:50 +0900

    Title: 음수 구별하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260102/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+
+    cout << N;
+    cout << "\n";
+    
+    if(N < 0) {
+        cout << "minus";
+    }
+
+    return 0;
+}
