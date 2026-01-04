8fdccee7148efcbc58f95d61dd5d07685cf8341f
RyuJaeHwi <91930771+RyuJaeHwi@users.noreply.github.com>
2026-01-04 20:30:08 +0900

    Title: N번 더하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260104/N번 더하기/add-n-times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, N;
+
+    cin >> A >> N;
+
+    for(int i = 0; i < N; i++){
+        A += N;
+        cout << A << "\n";
+    }
+
+    return 0;
+}
