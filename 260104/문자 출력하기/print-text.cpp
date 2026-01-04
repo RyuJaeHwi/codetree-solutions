bd0ebb0e8641070fb9d1bfe124ba8ca6bfa3e791
RyuJaeHwi <91930771+RyuJaeHwi@users.noreply.github.com>
2026-01-04 20:12:00 +0900

    Title: 문자 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260104/문자 출력하기/print-text.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char N;
+
+    cin >> N;
+
+    for(int i = 0; i < 8; i++){
+        cout << N;
+    }
+    
+    return 0;
+}
