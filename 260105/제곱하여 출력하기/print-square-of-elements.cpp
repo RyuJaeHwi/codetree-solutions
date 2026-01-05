b2933c28dcbca648e8be9f4916004979822ed65e
RyuJaeHwi <91930771+RyuJaeHwi@users.noreply.github.com>
2026-01-05 14:33:16 +0900

    Title: 제곱하여 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260105/제곱하여 출력하기/print-square-of-elements.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+
+    cin >> N;
+
+    int array[100];
+
+    for(int i = 0; i < N; i++) {
+        cin >> array[i];
+    }
+
+    for(int i = 0; i < N; i++) {
+        array[i] = array[i] * array[i];
+    }
+
+    for(int i = 0; i < N; i++){
+        cout << array[i] << " ";
+    }
+
+    return 0;
+}
