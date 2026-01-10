8c433ca95b69ac7b7eb43f529ef602ae51cbef09
RyuJaeHwi <91930771+RyuJaeHwi@users.noreply.github.com>
2026-01-10 18:04:11 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260110/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+
+    while(true){
+
+        cin >> N;
+
+        if(N == 0){
+            break;
+        }
+
+        cout << N << "\n";
+    }
+    
+    return 0;
+}
